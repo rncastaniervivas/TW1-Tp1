83455a1e94b724d6d5a9af95d940838f02c23ac6
AldanaDiFlavio <aldanadiflavio@live.com.ar>
2017-09-12 20:36:29 -0300

    Segundo Ejercicio
    Test que busca todas las farmacias de una calle

--- src/test/java/ar/edu/unlam/tallerweb1/FarmaciaTest.java
+++ src/test/java/ar/edu/unlam/tallerweb1/FarmaciaTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
+import ar.edu.unlam.tallerweb1.modelo.Direccion;
 import ar.edu.unlam.tallerweb1.modelo.Farmacia;
 
 public class FarmaciaTest extends SpringTest {
@@ -32,5 +33,40 @@ public class FarmaciaTest extends SpringTest {
 		assertThat(resultado).hasSize(2);
 	}
 	
-	
+	@Test
+	@Transactional
+	@Rollback(true)
+	public void testQueBuscaLasFarmaciasDeUnaCalle(){
+		Direccion calleSuipacha = new Direccion();
+		calleSuipacha.setCalle("Suipacha");
+		getSession().save(calleSuipacha);
+		
+		Direccion calleArieta = new Direccion();
+		calleArieta.setCalle("Arieta");
+		getSession().save(calleArieta);
+		
+		Farmacia farmacity = new Farmacia ("Farmacity", "Martes");
+		farmacity.setDireccion(calleSuipacha);
+		getSession().save(farmacity);
+		
+		Farmacia farmaciaDelOeste = new Farmacia ("Farmacia del Oeste", "Jueves");
+		farmaciaDelOeste.setDireccion(calleSuipacha);
+		getSession().save(farmaciaDelOeste);
+		
+		Farmacia farmaciaArieta = new Farmacia ("Farmacia Arieta", "Martes");
+		farmaciaArieta.setDireccion(calleArieta);
+		getSession().save(farmaciaArieta);
+		
+		Farmacia farmaciaAzul = new Farmacia ("Farmacia Azul", "Viernes");
+		farmaciaAzul.setDireccion(calleSuipacha);
+		getSession().save(farmaciaAzul);
+		
+		List<Farmacia> resultado = getSession().createCriteria(Farmacia.class)
+				.createAlias("direccion", "farmaDir")
+				.add(Restrictions.eq("farmaDir.calle", "Suipacha"))
+				.list();
+		
+		assertThat(resultado).hasSize(3);
+		
+	}
 }
