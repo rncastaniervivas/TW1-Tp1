57536629aee4254e36d1a482700dbbf8f06408af
Rocho <rncastaniervivas@hotmail.com.ar>
2017-09-12 18:48:09 -0300

    Creacion de clases
    Se crean las clases Barrio y Comuna necesarias para los test

---
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Comuna.java
@@ -0,0 +1,19 @@
+package ar.edu.unlam.tallerweb1.modelo;
+
+public class Comuna {
+	private long idComuna;
+	private String nombre;
+	
+	public long getIdComuna() {
+		return idComuna;
+	}
+	public void setIdComuna(Long idComuna) {
+		this.idComuna = idComuna;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
